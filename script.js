ef97ac216ac2120e3912c9304747c6d4a6c9272c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-10-10 10:50:01 +0530

    updated script.js to adust length of incoming data

--- script.js
+++ script.js
@@ -2,26 +2,19 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 async function GetNewQuote(){
         document.getElementById('quote-box').style.display = 'none';
         document.getElementById('loader').style.display = 'block';
-        
-
-        
-
+ 
         await delay(1000);
-
-
-        let num = parseInt(String(Math.random() * 1000));
 
         var response = await fetch("https://type.fit/api/quotes");
         var data = await response.json();
-
-
+        const len = Object.keys( data ).length;
+        let num = parseInt(Math.floor(Math.random() * (len)) + 1);
 
         document.getElementById('quote-box').style.display = 'block';
         document.getElementById('loader').style.display = 'none';
 
         document.getElementById('loader').style.display = 'none';
         document.getElementById('quote-box').style.display = 'block';
-
 
         document.getElementById('quote').innerHTML = data[num].text.trim();
         
